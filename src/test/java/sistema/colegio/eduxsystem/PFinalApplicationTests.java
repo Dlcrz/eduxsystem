b76e6c36c182013a481ebd7fbf0c0f471ae1692b
Angel De la cruz <139004309+Dlcrz@users.noreply.github.com>
2024-11-15 14:41:51 -0500

    Update PFinalApplicationTests.java

--- src/test/java/sistema/colegio/eduxsystem/PFinalApplicationTests.java
+++ src/test/java/sistema/colegio/eduxsystem/PFinalApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PFinalApplicationTests {
+class EduxsystemApplicationTests {
 
 	@Test
 	void contextLoads() {
